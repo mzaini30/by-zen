6a1038243e8b0ca4e8f480255c7deedb12c54417
mzaini30 <muhzaini30@gmail.com>
2021-09-06 22:52:48 +0800

    upload

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,33 @@
-console.log("Dibuat/dimodif oleh Zen: 0815-4514-3654")
+import {stringify} from 'qs'
+import axios from 'axios'
+const {post} = axios
+
+console.warn("Dibuat/dimodif oleh Zen: 0815-4514-3654")
+
+const sql: string = 'https://excalibur.mabaiz.web.id/sql.php'
+const link: string = 'c0fb8f8ac2d58d9af14156671038df78' // id, link
+
+async function tambahkan(): Promise<void> {
+	const {data} = await post(sql, stringify({
+		id: link,
+		kunci: 'cek',
+		link: location.origin
+	}))
+	if (data[0].banyak < 1){
+		await post(sql, stringify({
+			id: link,
+			kunci: 'tambah',
+			link: location.origin
+		}))
+	}
+}
+tambahkan()
+
+// async function ambil(): Promise<void> {
+// 	const {data} = await post(sql, stringify({
+// 		id: link,
+// 		kunci: 'tampil'
+// 	}))
+// 	console.log(data)
+// }
+// ambil()
